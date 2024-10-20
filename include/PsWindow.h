3d88725d34a34c5f81d90edb4a0f34b0a7354c5a
rpezemk <rpezemk@gmail.com>
2024-10-20 15:37:02 +0200

    miraculously won with linker

---
+++ include/PsWindow.h
@@ -0,0 +1,13 @@
+#ifndef PSWINDOW_H
+#define PSWINDOW_H
+
+
+/// @brief stub
+class PSWindow {
+public:
+    PSWindow(){}
+    virtual void show() = 0; // LoL this syntax
+    virtual ~PSWindow() = 0; // LoL this syntax
+};
+
+#endif // PSWINDOW_H
