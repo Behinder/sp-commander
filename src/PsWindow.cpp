d8394dd9eeb740a5a19c154e869d3688f28125bd
rpezemk <rpezemk@gmail.com>
2024-10-20 14:35:54 +0200

    removed unused classes

--- src/PsWindow.cpp
+++ src/PsWindow.cpp
@@ -2,13 +2,6 @@
 #include <vector>
 #include <string>
 
-class PSStatusBar{
-
-};
-class PSMenuBar{
-
-};
-
 class PSWindow {
 public:
     char* title;
@@ -18,44 +11,4 @@ public:
     };
 
     virtual ~PSWindow() = default;
-};
-
-class ExplorerWindow : public PSWindow {
-    public:
-    ExplorerWindow(const std::string& title) : PSWindow(title) {}
-
-    void show() override {
-        PSWindow::show();
-    }
-};
-
-class EditorWindow : public PSWindow{
-    public:
-    EditorWindow(const std::string& title) : PSWindow(title) {}
-
-    void show() override {
-        PSWindow::show();
-    }
-};
-
-class TiledView {
-public:
-    std::vector<PSWindow> windows; // Vector of PSWindow
-
-    void addWindow(PSWindow& window) {
-        windows.push_back(window);
-    };
-
-    void showAllWindows(){
-        for (PSWindow& window : windows) {
-        window.show();
-        }
-    };
-};
-
-class PSScreen{
-    public:
-    PSMenuBar MenuBar;
-    TiledView MainView;
-    PSStatusBar StatusBar;
 };
